2574cd09a6bc65c26b1b7bf19505a19176de5077
TariQMah <t.ali1993@hotmail.com>
2023-08-13 03:14:31 +0500

    basic layout ready

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+async function Page() {
+    return (
+        <main>
+            <h1>1sdasad</h1>
+        </main>
+    )
+
+}
+
+export default Page
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,6 @@
+
+function RightSidebar() {
+    return <h1>Right</h1>
+}
+
+export default RightSidebar
