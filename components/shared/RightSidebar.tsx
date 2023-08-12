316eeb851b293aed509a8cc5b25fdb4495d3b186
TariQMah <t.ali1993@hotmail.com>
2023-08-13 04:20:10 +0500

    onbording route UI completed

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,17 @@
 
 function RightSidebar() {
-    return <h1>Right</h1>
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communites</h3>
+
+            </div>
+
+            <div className="flex flex-1 flex-col">
+                <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+            </div>
+        </section>
+    )
 }
 
 export default RightSidebar
